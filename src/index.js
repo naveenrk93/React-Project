314b25501245382b0c1b2d4b18c1c468a11850a2
Naveen Ramkumar <naveenrk93@gmail.com>
2018-05-21 14:48:41 -0700

    Defining Components and adding them to main page

--- src/index.js
+++ src/index.js
@@ -1,22 +1,44 @@
-import React from 'react';
+import React , { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './Components/search-bar';
+import VideoList from './Components/video-list';
+import VideoDetail from './Components/video-detail';
 
 const API_KEY = 'AIzaSyDCAsX-fCVB0C4-gOutQDGSSJDiK9Syl_U';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, (data) =>{
-    console.log(data);
-});
+class App extends Component
+{
+    constructor(props){
+        super(props);
 
-const App = function(){
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
-};
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render(){
+        return(
+            <div>
+                <SearchBar />
+                <VideoDetail video={ this.state.selectedVideo }/>
+                <VideoList
+                    onVideoSelect = { selectedVideo => {this.setState({ selectedVideo })} }
+                    videos={ this.state.videos }
+                />
+            </div>
+        );
+    }
+}
 
 
 
